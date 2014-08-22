3a84ab0bc468f35d7b10de94b97b419eb705c9ca
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2014-08-22 00:26:49 -0700

    Use low byte in name, not high.

--- BLE-CC254x-1.4.0/Projects/ble/Profiles/Roles/peripheralObserver.c
+++ BLE-CC254x-1.4.0/Projects/ble/Profiles/Roles/peripheralObserver.c
@@ -972,8 +972,8 @@ static void gapRole_ProcessGAPMsg( gapEventHdr_t *pMsg )
           // This should match the original advertising setup in BlueBasic.c:consoleAdvert
           if (gapRole_AdvertDataLen == 31 && osal_memcmp(&gapRole_AdvertData[23], "BASIC#??", 8))
           {
-            gapRole_AdvertData[29] = (gapRole_bdAddr[5] >> 4) + (gapRole_bdAddr[5] >= 0xA0 ? 'A' - 0xA : '0');
-            gapRole_AdvertData[30] = (gapRole_bdAddr[5] & 15) + ((gapRole_bdAddr[5] & 15) >= 0x0A ? 'A' - 0xA : '0');
+            gapRole_AdvertData[29] = (gapRole_bdAddr[0] >> 4) + (gapRole_bdAddr[0] >= 0xA0 ? 'A' - 0xA : '0');
+            gapRole_AdvertData[30] = (gapRole_bdAddr[0] & 15) + ((gapRole_bdAddr[0] & 15) >= 0x0A ? 'A' - 0xA : '0');
           }
 
           // Update the advertising data
