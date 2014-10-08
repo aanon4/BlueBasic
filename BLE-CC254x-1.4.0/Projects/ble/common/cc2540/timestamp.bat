ae82d76e146306bf1841e641a826e101804b510e
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2014-10-08 11:57:18 -0700

    Added build versions to Hex directory
    Build versions are part of the auto-update feature coming soon.

--- BLE-CC254x-1.4.0/Projects/ble/common/cc2540/timestamp.bat
+++ BLE-CC254x-1.4.0/Projects/ble/common/cc2540/timestamp.bat
@@ -1,4 +1,6 @@
 @echo off
 set hour=%time:~0,2%
 if %hour% lss 10 (set hour=0%time:~1,1%)
-echo #define BUILD_TIMESTAMP "%1/%date:~-4%%date:~4,2%%date:~7,2%%hour%%time:~3,2%%time:~6,2%" > "%2\timestamp.h"
+set timestamp=%date:~-4%%date:~4,2%%date:~7,2%%hour%%time:~3,2%%time:~6,2%
+echo #define BUILD_TIMESTAMP "%2/%timestamp%" > "%3/timestamp.h"
+echo %timestamp% > "%1\..\..\..\..\..\hex\BlueBasic-%2.version"
